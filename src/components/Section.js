2cf7f5b2a8fa3b8836173a5a256c792c41d7ffe7
Mehdi Salimi <Salimi.devop@gmail.com>
2022-09-26 23:10:14 +0330

    passing props in section to create different details in each page

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function Section() {
+function Section(props) {
+    const { title, description, backgroundImg, leftBtnText, rightBtnText } = props;
+
     return (
-        <Wrap>
+        <Wrap bgUrl={backgroundImg}>
             <ItemText>
-                <h1>Model S</h1>
-                <p>Order Online for Touchless Delivery</p>
+                <h1>{title}</h1>
+                <p>{description}</p>
             </ItemText>
             <Buttons>
                 <ButtonGroup>
-                    <LeftButton>Custom Order</LeftButton>
-                    <RightButton>Existing Inventory</RightButton>
+                    <LeftButton>{leftBtnText}</LeftButton>
+                    {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
                 </ButtonGroup>
                 <DownArrow src='/images/down-arrow.svg' />
             </Buttons>
@@ -27,7 +29,7 @@ const Wrap = styled.div`
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
-    background-image: url('/images/model-s.jpg');
+    background-image: ${props => `url(/images/${props.bgUrl})`};
     display: flex;
     flex-direction: column;
     justify-content: space-between;
