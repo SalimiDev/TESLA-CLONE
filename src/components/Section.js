dbff9fcd987cfe5d58773a4b751fe24a6229015e
Mehdi Salimi <Salimi.devop@gmail.com>
2022-09-28 05:52:36 +0330

    fade animation added

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import Fade from 'react-reveal/Fade';
 
 function Section(props) {
     const { title, description, backgroundImg, leftBtnText, rightBtnText } = props;
 
     return (
         <Wrap bgUrl={backgroundImg}>
-            <ItemText>
-                <h1>{title}</h1>
-                <p>{description}</p>
-            </ItemText>
+            <Fade bottom>
+                <ItemText>
+                    <h1>{title}</h1>
+                    <p>{description}</p>
+                </ItemText>
+            </Fade>
             <Buttons>
-                <ButtonGroup>
-                    <LeftButton>{leftBtnText}</LeftButton>
-                    {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
-                </ButtonGroup>
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>{leftBtnText}</LeftButton>
+                        {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+                    </ButtonGroup>
+                </Fade>
                 <DownArrow src='/images/down-arrow.svg' />
             </Buttons>
         </Wrap>
